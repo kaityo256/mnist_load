cc8137236a6e29c0dad656a6ee9ea3464cca5c23
H. Watanabe <kaityo@users.sourceforge.jp>
2024-07-04 13:29:03 +0900

    :memo: updates

--- load.cpp
+++ load.cpp
@@ -2,8 +2,10 @@
 #include <fstream>
 #include <iostream>
 
-static uint8_t train_data[60000][28][28];
+static uint8_t train_label[60000];
+static uint8_t test_label[10000];
 static uint8_t test_data[10000][28][28];
+static uint8_t train_data[60000][28][28];
 
 void show() {
   for (int i = 0; i < 10; i++) {
@@ -20,16 +22,17 @@ void show() {
   }
 }
 
-void load_file(const char *filename) {
+void load_file(const char *filename, char *data) {
   std::ifstream file(filename, std::ios::binary);
   file.seekg(0, std::ios::end);
   std::streamsize size = file.tellg();
   file.seekg(0, std::ios::beg);
   std::size_t filesize = size / sizeof(uint8_t);
-  file.read(reinterpret_cast<char *>(train_data), filesize);
+  file.read(data, filesize);
 }
 
 int main() {
-  load_file("mnist_train_data.dat");
+  load_file("mnist_train_data.dat", reinterpret_cast<char *>(train_data));
+  load_file("mnist_train_label.dat", reinterpret_cast<char *>(train_label));
   show();
 }
